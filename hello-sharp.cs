d50926f2f9fece8801fec5794687e863270f7b1b
Isaac Evans <I.Evans2010@proton.me>
2025-10-01 23:24:03 +0100

    Add loads

---
+++ hello-sharp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldApp {
+  class Hello{
+    static void main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      Console.ReadKey();
+    }
+  }
+
+}
